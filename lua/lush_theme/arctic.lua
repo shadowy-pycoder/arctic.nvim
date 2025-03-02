4e7abd6aaccb7eb933f2874ff6819af77c28f6bc
shadowy-pycoder <35629483+shadowy-pycoder@users.noreply.github.com>
2025-03-02 10:14:59 +0300

    color rethinking

--- lua/lush_theme/arctic.lua
+++ lua/lush_theme/arctic.lua
@@ -53,6 +53,12 @@ local pine = "#3e8fb0"
 local leaf = "#95b1ac"
 local subtle = "#908caa"
 
+-- vscode colors overrides
+local override_blue_green = "#EBC06D"
+local override_light_blue = "#c1b9b5"
+local override_dark_blue = "#598489"
+local override_dark_pink = "#598489"
+
 ---@diagnostic disable
 local theme = lush(function(injected_functions)
 	local sym = injected_functions.sym
@@ -87,7 +93,7 @@ local theme = lush(function(injected_functions)
 		-- For the unused code, use Identifier's fg (9cdcfe) as the base color,
 		-- editorUnnecessaryCode.opacity is 000000aa (the alpha value is aa),
 		-- so the color will be 9cdcfeaa. Converting hexa to hex gets 729db4.
-		UnnecessaryCode({ fg = "#729db4" }),
+		UnnecessaryCode({ fg = "#9d8049" }),
 		-- Git diff
 		DiffTextAdded({ bg = "#214d29" }), -- diffEditor.insertedTextBackground (DiffLineAdded as its background)
 		DiffTextDeleted({ bg = "#712928" }), -- diffEditor.removedTextBackground (DiffLineDeleted as its background)
@@ -179,31 +185,31 @@ local theme = lush(function(injected_functions)
 		--
 		Comment({ fg = green }),
 
-		Constant({ fg = dark_blue }),
+		Constant({ fg = override_dark_blue }),
 		String({ fg = brown }),
 		Character({ Constant }),
 		Number({ fg = light_green }),
 		Boolean({ Constant }),
 		Float({ Number }),
 
-		Identifier({ fg = light_blue }),
-		Function({ fg = yellow }),
+		Identifier({ fg = override_light_blue }),
+		Function({ fg = override_light_blue }),
 
-		Statement({ fg = dark_pink }),
+		Statement({ fg = override_dark_pink }),
 		Conditional({ Statement }),
 		Repeat({ Statement }),
 		Label({ Statement }),
 		Operator({ fg = norm_fg }),
-		Keyword({ fg = dark_pink }),
+		Keyword({ fg = override_dark_pink }),
 		Exception({ Statement }),
 
-		PreProc({ fg = dark_pink }),
+		PreProc({ fg = override_dark_pink }),
 		Include({ PreProc }),
 		Define({ PreProc }),
 		Macro({ PreProc }),
 		PreCondit({ PreProc }),
 
-		Type({ fg = dark_blue }),
+		Type({ fg = override_dark_blue }),
 		StorageClass({ Type }),
 		Structure({ Type }),
 		Typedef({ Type }),
@@ -316,17 +322,17 @@ local theme = lush(function(injected_functions)
 		-- To find all the capture names, see https://github.com/nvim-treesitter/nvim-treesitter/blob/master/CONTRIBUTING.md#highlights)
 
 		-- Identifiers
-		sym("@variable")({ fg = light_blue }), -- various variable names
-		sym("@variable.builtin")({ fg = light_blue }), -- built-in variable names (e.g. `this`)
-		sym("@variable.parameter")({ fg = light_blue }), -- parameters of a function, use a conspicuous color (VSCode uses the commo= light_blue)
+		sym("@variable")({ fg = override_light_blue }), -- various variable names
+		sym("@variable.builtin")({ fg = override_light_blue }), -- built-in variable names (e.g. `this`)
+		sym("@variable.parameter")({ fg = override_light_blue }), -- parameters of a function, use a conspicuous color (VSCode uses the commo= light_blue)
 		sym("@variable.parameter.builtin")({ sym("@variable.parameter") }), -- special parameters (e.g. `_`, `it`)
-		sym("@variable.member")({ fg = light_blue }), -- object and struct fields
+		sym("@variable.member")({ fg = override_light_blue }), -- object and struct fields
 
-		sym("@constant")({ fg = blue }), -- constant identifiers
+		sym("@constant")({ fg = override_light_blue }), -- constant identifiers
 		sym("@constant.builtin")({ Constant }), -- built-in constant values
 		sym("@constant.macro")({ Constant }), -- constants defined by the preprocessor
 
-		sym("@module")({ fg = blue_green }), -- modules or namespaces
+		sym("@module")({ fg = override_blue_green }), -- modules or namespaces
 		sym("@module.builtin")({ sym("@module") }), -- built-in modules or namespaces
 		sym("@label")({ fg = label_fg }), -- GOTO and other labels (e.g. `label:` in C), including heredoc labels
 
@@ -348,12 +354,12 @@ local theme = lush(function(injected_functions)
 		sym("@number.float")({ Float }), -- floating-point number literals
 
 		-- Types
-		sym("@type")({ fg = blue_green }), -- type or class definitions and annotations
-		sym("@type.builtin")({ fg = blue_green }), -- built-in types
-		sym("@type.definition")({ fg = blue_green }), -- identifiers in type definitions (e.g. `typedef <type> <identifier>` in C)
+		sym("@type")({ fg = override_blue_green }), -- type or class definitions and annotations
+		sym("@type.builtin")({ fg = override_blue_green }), -- built-in types
+		sym("@type.definition")({ fg = override_blue_green }), -- identifiers in type definitions (e.g. `typedef <type> <identifier>` in C)
 
-		sym("@attribute")({ fg = yellow }), -- attribute annotations (e.g. Python decorators)
-		sym("@attribute.builtin")({ fg = blue_green }), -- builtin annotations (e.g. `@property` in Python)
+		sym("@attribute")({ fg = override_blue_green }), -- attribute annotations (e.g. Python decorators)
+		sym("@attribute.builtin")({ fg = override_blue_green }), -- builtin annotations (e.g. `@property` in Python)
 		sym("@property")({ sym("@variable.member") }), -- the key in key/value pairs
 
 		-- Function
@@ -363,21 +369,21 @@ local theme = lush(function(injected_functions)
 		sym("@function.macro")({ Function }), -- preprocessor macros
 
 		sym("@function.method")({ sym("@function") }), -- method definitions
-		sym("@function.method.call")({ sym("@function.call") }), -- method calls
+		sym("@function.method.call")({ fg=override_light_blue }), -- method calls
 
-		sym("@constructor")({ fg = blue_green }), -- constructor calls and definitions
+		sym("@constructor")({ fg = override_blue_green }), -- constructor calls and definitions
 		sym("@operator")({ Operator }), -- symbolic operators (e.g. `+` / `*`)
 
 		-- Keyword
 		sym("@keyword")({ Keyword }), -- keywords not fitting into specific categories
-		sym("@keyword.coroutine")({ fg = dark_pink }), -- keywords related to coroutines (e.g. `go` in Go, `async/await` in Python)
-		sym("@keyword.function")({ fg = dark_blue }), -- keywords that define a function (e.g. `func` in Go, `def` in Python)
-		sym("@keyword.operator")({ fg = dark_blue }), -- operators that are English words (e.g. `and` / `or`)
+		sym("@keyword.coroutine")({ fg = override_dark_pink }), -- keywords related to coroutines (e.g. `go` in Go, `async/await` in Python)
+		sym("@keyword.function")({ fg = override_dark_blue }), -- keywords that define a function (e.g. `func` in Go, `def` in Python)
+		sym("@keyword.operator")({ fg = override_dark_blue }), -- operators that are English words (e.g. `and` / `or`)
 		sym("@keyword.import")({ Include }), -- keywords for including modules (e.g. `import` / `from` in Python)
-		sym("@keyword.type")({ fg = dark_blue }), -- keywords describing composite types (e.g. `struct`, `enum`)
-		sym("@keyword.modifier")({ fg = dark_blue }), -- keywords modifying other constructs (e.g. `const`, `static`, `public`)
+		sym("@keyword.type")({ fg = override_dark_blue }), -- keywords describing composite types (e.g. `struct`, `enum`)
+		sym("@keyword.modifier")({ fg = override_dark_blue }), -- keywords modifying other constructs (e.g. `const`, `static`, `public`)
 		sym("@keyword.repeat")({ Repeat }), -- keywords related to loops (e.g. `for` / `while`)
-		sym("@keyword.return")({ fg = dark_pink }), --  keywords like `return` and `yield`
+		sym("@keyword.return")({ fg = override_dark_pink }), --  keywords like `return` and `yield`
 		sym("@keyword.debug")({ Debug }), -- keywords related to debugging
 		sym("@keyword.exception")({ Exception }), -- keywords related to exceptions (e.g. `throw` / `catch`)
 
@@ -417,7 +423,7 @@ local theme = lush(function(injected_functions)
 		sym("@markup.heading.6")({ sym("@markup.heading") }),
 
 		sym("@markup.quote")({ fg = green }), -- block quotes
-		sym("@markup.math")({ fg = blue_green }), -- math environments (e.g. `$ ... $` in LaTeX)
+		sym("@markup.math")({ fg = override_blue_green }), -- math environments (e.g. `$ ... $` in LaTeX)
 
 		sym("@markup.link")({ fg = brown }), -- text references, footnotes, citations, etc.
 		sym("@markup.link.label")({ sym("@markup.link") }), -- non-url links
@@ -434,14 +440,14 @@ local theme = lush(function(injected_functions)
 		sym("@diff.minus")({ DiffTextDeleted }), -- deleted text (for diff files)
 		sym("@diff.delta")({ DiffTextChanged }), -- changed text (for diff files)
 
-		sym("@tag")({ fg = dark_blue }), -- XML tag names
+		sym("@tag")({ fg = override_dark_blue }), -- XML tag names
 		sym("@tag.builtin")({ sym("@tag") }), -- builtin tag names (e.g. HTML5 tags)
-		sym("@tag.attribute")({ fg = light_blue }), -- XML tag attributes
+		sym("@tag.attribute")({ fg = override_light_blue }), -- XML tag attributes
 		sym("@tag.delimiter")({ fg = gray3 }), -- XML tag delimiters
 
 		-- Language specific
 		-- Lua
-		sym("@variable.member.lua")({ fg = blue_green }),
+		sym("@variable.member.lua")({ fg = override_blue_green }),
 
 		--
 		-- LSP semantic tokens
@@ -472,7 +478,7 @@ local theme = lush(function(injected_functions)
 		sym("@lsp.type.operator")({ sym("@operator") }),
 		sym("@lsp.type.decorator")({ sym("@attribute") }),
 		sym("@lsp.type.escapeSequence")({ sym("@string.escape") }),
-		sym("@lsp.type.formatSpecifier")({ fg = light_blue }),
+		sym("@lsp.type.formatSpecifier")({ fg = override_light_blue }),
 		sym("@lsp.type.builtinType")({ sym("@type.builtin") }),
 		sym("@lsp.type.typeAlias")({ sym("@type.definition") }),
 		sym("@lsp.type.unresolvedReference")({ gui = "undercurl", sp = error_red }),
@@ -873,7 +879,7 @@ local theme = lush(function(injected_functions)
 		-- Rainbow delimiters
 
 		DelimOrange({ fg = "#ffcc00" } ),
-		DelimPink({ fg = dark_pink } ),
+		DelimPink({ fg = override_dark_pink } ),
 		DelimBlue({ fg = blue } ),
 
 		-- yaml
@@ -888,4 +894,3 @@ end)
 ---@diagnostic enable
 
 return theme
-
